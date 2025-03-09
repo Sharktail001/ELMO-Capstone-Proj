961baadfc5d668913278d58336f01f00f0d2daa1
Abel Thomas <109200318+Sharktail001@users.noreply.github.com>
2025-03-09 00:33:52 -0600

    Fixed merge conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 // import Image from "next/image";
 // import Dashboard from "./dashboard/page";
-import LoginPage from "./login/page";
+import LoginPage from "./authentication/page";
 
 export default function Home() {
   return <LoginPage />;
