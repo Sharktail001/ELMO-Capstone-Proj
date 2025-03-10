814ddfe00445b95e56a77c8a78a6ba8f0ca16cba
Shaz Kumar <shazk@Shazs-MacBook-Pro.local>
2025-03-09 23:15:03 -0500

    working dashboard and AI

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,5 @@
+import tailwindcssAnimate from "tailwindcss-animate";
+import tailwindcssTypography from "@tailwindcss/typography";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -70,5 +72,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate, tailwindcssTypography],
 } satisfies Config;
